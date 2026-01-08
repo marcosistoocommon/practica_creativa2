32593e994036bf95b077d57417717685976761e6
marcosistoocommon <maperu2004@gmail.com>
2026-01-08 12:34:51 +0100

    Name change

--- script_1.py
+++ script_1.py
@@ -6,7 +6,7 @@ subprocess.run("sudo apt update", shell=True)
 subprocess.run("sudo apt install software-properties-common", shell=True)
 subprocess.run("sudo add-apt-repository -y ppa:deadsnakes/ppa", shell=True)
 subprocess.run("sudo apt install -y python3.9 python3.9-venv python3.9-distutils", shell=True)
-os.chdir("practica_creativa2/bookinfo/src/productpage/")
+os.chdir("parte_1/bookinfo/src/productpage/")
 
 subprocess.run("python3.9 -m venv venv", shell=True)
 subprocess.run("./venv/bin/pip install -r requirements.txt", shell=True)
