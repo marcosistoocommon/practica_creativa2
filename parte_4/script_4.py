eec275a2425260acaf11885ecbc20c8eddaf8fba
marcosistoocommon <maperu2004@gmail.com>
2026-01-19 18:51:43 +0100

    script 5.1

--- parte_4/script_4.py
+++ parte_4/script_4.py
@@ -1,39 +1,24 @@
 #!/usr/bin/env python3
 import subprocess, os
 
-# Get the directory where the script is located
-script_dir = os.path.dirname(os.path.abspath(__file__))
-kube_dir = os.path.join(script_dir, "bookinfo/platform/kube")
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-subprocess.run("eval $(minikube docker-env)", shell=True)
-
-
-os.chdir(os.path.join(script_dir, "bookinfo/src/reviews"))
-subprocess.run(f"docker run --rm -u root -v {os.getcwd()}:/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build", shell=True)
-os.chdir(script_dir)
-
-subprocess.run("docker build -f Dockerfile.productpage -t 17/productpage .", shell=True)
-subprocess.run("docker build -f Dockerfile.details -t 17/details bookinfo/src/details", shell=True)
-subprocess.run("docker build -f Dockerfile.ratings -t 17/ratings --build-arg star_color=black bookinfo/src/ratings", shell=True)
-
-subprocess.run("docker build -f bookinfo/src/reviews/reviews-wlpcfg/Dockerfile -t 17/reviews-v1 --build-arg service_version=v1 --build-arg enable_ratings=false bookinfo/src/reviews/reviews-wlpcfg", shell=True)
-subprocess.run("docker build -f bookinfo/src/reviews/reviews-wlpcfg/Dockerfile -t 17/reviews-v2 --build-arg service_version=v2 --build-arg enable_ratings=true --build-arg star_color=black bookinfo/src/reviews/reviews-wlpcfg", shell=True)
-subprocess.run("docker build -f bookinfo/src/reviews/reviews-wlpcfg/Dockerfile -t 17/reviews-v3 --build-arg service_version=v3 --build-arg enable_ratings=true --build-arg star_color=red bookinfo/src/reviews/reviews-wlpcfg", shell=True)
-
-
-manifests = [
-    'cdps-namespace.yaml',
-    'details.yaml',
-    'ratings.yaml',
-    'reviews-svc.yaml',
-    'reviews-v1-deployment.yaml',
-    'reviews-v2-deployment.yaml',
-    'reviews-v3-deployment.yaml',
-    'productpage.yaml'
-]
-
-for manifest in manifests:
-    manifest_path = os.path.join(kube_dir, manifest)
-    subprocess.run(f"kubectl apply -f {manifest_path} --validate=false", shell=True)
-
-subprocess.run("kubectl port-forward --address=0.0.0.0 -n cdps-17 svc/productpage-service 9080:9080", shell=True)
+subprocess.run("""
+eval $(minikube docker-env)
+cd bookinfo/src/reviews && docker run --rm -u root -v $(pwd):/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build && cd ../../..
+docker build -f Dockerfile.productpage -t 17/productpage .
+docker build -f Dockerfile.details -t 17/details .
+docker build -f Dockerfile.ratings -t 17/ratings --build-arg star_color=black .
+docker build -f bookinfo/src/reviews/reviews-wlpcfg/Dockerfile -t 17/reviews-v1 --build-arg service_version=v1 --build-arg enable_ratings=false .
+docker build -f bookinfo/src/reviews/reviews-wlpcfg/Dockerfile -t 17/reviews-v2 --build-arg service_version=v2 --build-arg enable_ratings=true --build-arg star_color=black .
+docker build -f bookinfo/src/reviews/reviews-wlpcfg/Dockerfile -t 17/reviews-v3 --build-arg service_version=v3 --build-arg enable_ratings=true --build-arg star_color=red .
+kubectl apply -f bookinfo/platform/kube/cdps-namespace.yaml --validate=false
+kubectl apply -f bookinfo/platform/kube/details.yaml --validate=false
+kubectl apply -f bookinfo/platform/kube/ratings.yaml --validate=false
+kubectl apply -f bookinfo/platform/kube/reviews-svc.yaml --validate=false
+kubectl apply -f bookinfo/platform/kube/reviews-v1-deployment.yaml --validate=false
+kubectl apply -f bookinfo/platform/kube/reviews-v2-deployment.yaml --validate=false
+kubectl apply -f bookinfo/platform/kube/reviews-v3-deployment.yaml --validate=false
+kubectl apply -f bookinfo/platform/kube/productpage.yaml --validate=false
+kubectl port-forward --address=0.0.0.0 -n cdps-17 svc/productpage-service 9080:9080
+""", shell=True, executable="/bin/bash")
