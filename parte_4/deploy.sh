418673b1a456544d22e053ee01cf34de3d078e5d
marcosistoocommon <maperu2004@gmail.com>
2026-01-19 19:22:43 +0100

    b

--- parte_4/deploy.sh
+++ parte_4/deploy.sh
@@ -3,23 +3,23 @@
 SCRIPT_DIR="$(pwd)"
 
 eval $(minikube docker-env)
-cd "$SCRIPT_DIR/../../.." || exit 1
-docker build -f Dockerfile.productpage -t 17/productpage . || true
-docker build -f Dockerfile.details -t 17/details . || true
-docker build -f Dockerfile.ratings -t 17/ratings . || true
+cd "$SCRIPT_DIR/../../.." 
+docker build -f Dockerfile.productpage -t 17/productpage . 
+docker build -f Dockerfile.details -t 17/details . 
+docker build -f Dockerfile.ratings -t 17/ratings . 
 cd bookinfo/src/reviews
 
 docker run --rm -u root -v "$(pwd)":/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build
 
 cd ../../..
-docker build -t 17/reviews-v1 --build-arg service_version=v1 --build-arg enable_ratings=false bookinfo/src/reviews/reviews-wlpcfg || true
-docker build -t 17/reviews-v2 --build-arg service_version=v2 --build-arg enable_ratings=true --build-arg star_color=black bookinfo/src/reviews/reviews-wlpcfg || true
-docker build -t 17/reviews-v3 --build-arg service_version=v3 --build-arg enable_ratings=true --build-arg star_color=red bookinfo/src/reviews/reviews-wlpcfg || true
+docker build -t 17/reviews-v1 --build-arg service_version=v1 --build-arg enable_ratings=false bookinfo/src/reviews/reviews-wlpcfg 
+docker build -t 17/reviews-v2 --build-arg service_version=v2 --build-arg enable_ratings=true --build-arg star_color=black bookinfo/src/reviews/reviews-wlpcfg 
+docker build -t 17/reviews-v3 --build-arg service_version=v3 --build-arg enable_ratings=true --build-arg star_color=red bookinfo/src/reviews/reviews-wlpcfg 
 cd "$SCRIPT_DIR"
 
 
 kubectl apply -f cdps-namespace.yaml
-sleep 2
+
 kubectl apply -f details.yaml
 kubectl apply -f ratings.yaml
 kubectl apply -f reviews-svc.yaml
