cdb9ff174721466ccadd02bc177c5d08dad2399b
marcosistoocommon <maperu2004@gmail.com>
2026-01-19 12:43:44 +0100

    Practica 3 presentable

---
+++ parte_3/script_3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import subprocess, os
+import sys
+from pathlib import Path
+
+os.chdir("bookinfo/src/reviews")
+subprocess.run("docker run --rm -u root -v '$(pwd)':/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build", shell=True)
+os.chdir("../../..")
+subprocess.run("docker compose -f docker-compose.micro.yml build", shell=True)
+subprocess.run("docker compose -f docker-compose.micro.yml up -d", shell=True)
