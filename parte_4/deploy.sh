56c0e7cb7cba6ba4f0ddf06b79a0d7d4b1153219
marcosistoocommon <maperu2004@gmail.com>
2026-01-19 18:57:58 +0100

    fix deploy

--- parte_4/deploy.sh
+++ parte_4/deploy.sh
@@ -6,9 +6,9 @@ cd bookinfo/src/reviews && docker run --rm -u root -v $(pwd):/home/gradle/projec
 docker build -f Dockerfile.productpage -t 17/productpage .
 docker build -f Dockerfile.details -t 17/details .
 docker build -f Dockerfile.ratings -t 17/ratings --build-arg star_color=black .
-docker build -f bookinfo/src/reviews/reviews-wlpcfg/Dockerfile -t 17/reviews-v1 --build-arg service_version=v1 --build-arg enable_ratings=false .
-docker build -f bookinfo/src/reviews/reviews-wlpcfg/Dockerfile -t 17/reviews-v2 --build-arg service_version=v2 --build-arg enable_ratings=true --build-arg star_color=black .
-docker build -f bookinfo/src/reviews/reviews-wlpcfg/Dockerfile -t 17/reviews-v3 --build-arg service_version=v3 --build-arg enable_ratings=true --build-arg star_color=red .
+docker build -f bookinfo/src/reviews/reviews-wlpcfg/Dockerfile -t 17/reviews-v1 --build-arg service_version=v1 --build-arg enable_ratings=false bookinfo/src/reviews
+docker build -f bookinfo/src/reviews/reviews-wlpcfg/Dockerfile -t 17/reviews-v2 --build-arg service_version=v2 --build-arg enable_ratings=true --build-arg star_color=black bookinfo/src/reviews
+docker build -f bookinfo/src/reviews/reviews-wlpcfg/Dockerfile -t 17/reviews-v3 --build-arg service_version=v3 --build-arg enable_ratings=true --build-arg star_color=red bookinfo/src/reviews
 kubectl apply -f bookinfo/platform/kube/cdps-namespace.yaml --validate=false
 kubectl apply -f bookinfo/platform/kube/details.yaml --validate=false
 kubectl apply -f bookinfo/platform/kube/ratings.yaml --validate=false
