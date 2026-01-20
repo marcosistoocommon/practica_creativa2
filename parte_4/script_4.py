fe7718994b98ba17b440025c17ad7d074ffc20d0
marcosistoocommon <maperu2004@gmail.com>
2026-01-20 10:56:17 +0100

    attempt with minikube

--- parte_4/script_4.py
+++ parte_4/script_4.py
@@ -1,5 +1,37 @@
 #!/usr/bin/env python3
 import subprocess, os
+import sys
+from pathlib import Path
 
-subprocess.run("chmod +x bookinfo/platform/kube/deploy.sh", shell=True)
-subprocess.run("bookinfo/platform/kube/deploy.sh", shell=True)
+subprocess.run("export DEBIAN_FRONTEND=noninteractive && sudo apt-get update -y", shell=True)
+subprocess.run("export DEBIAN_FRONTEND=noninteractive && sudo apt install -y curl wget apt-transport-https", shell=True)
+subprocess.run("echo 'virtualbox-ext-pack virtualbox-ext-pack/license select true' | sudo debconf-set-selections", shell=True)
+subprocess.run("export DEBIAN_FRONTEND=noninteractive && sudo apt install -y virtualbox virtualbox-ext-pack", shell=True)
+subprocess.run("curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64", shell=True)
+subprocess.run("sudo install minikube-linux-amd64 /usr/local/bin/minikube", shell=True)
+subprocess.run("minikube start --driver=virtualbox --force", shell=True)
+subprocess.run("sudo snap install kubectl --classic", shell=True)
+
+os.chdir("bookinfo/src/reviews")
+pwd = os.getcwd()
+subprocess.run(f"docker run --rm -u root -v {pwd}:/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build", shell=True)
+os.chdir("reviews-wlpgc")
+subprocess.run("docker build -t 17/reviews-v1 --build-arg service_version=v1 --build-arg enable_ratings=false .", shell=True)
+subprocess.run("docker build -t 17/reviews-v2 --build-arg service_version=v2 --build-arg enable_ratings=true --build-arg star_color=black .", shell=True)
+subprocess.run("docker build -t 17/reviews-v3 --build-arg service_version=v3 --build-arg enable_ratings=true --build-arg star_color=red .", shell=True)
+os.chdir("../../../..")
+subprocess.run("docker build -f Dockerfile.productpage -t 17/productpage .", shell=True)
+subprocess.run("docker build -f Dockerfile.ratings -t 17/ratings .", shell=True)
+subprocess.run("docker build -f Dockerfile.details -t 17/details .", shell=True)
+
+os.chdir("../../parte_4/bookinfo/platform/kube")
+
+subprocess.run("kubectl apply -f cdps-namespace.yaml", shell=True)
+subprocess.run("kubectl apply -f details.yaml", shell=True)
+subprocess.run("kubectl apply -f ratings.yaml", shell=True)
+subprocess.run("kubectl apply -f reviews-svc.yaml", shell=True)
+subprocess.run("kubectl apply -f reviews-v1.yaml", shell=True)
+subprocess.run("kubectl apply -f reviews-v2.yaml", shell=True)
+subprocess.run("kubectl apply -f reviews-v3.yaml", shell=True)
+subprocess.run("kubectl apply -f productpage.yaml", shell=True)
+
