afdc0b513d84ef325d0422ceb1d016d3aa964f19
marcosistoocommon <maperu2004@gmail.com>
2026-01-20 11:13:52 +0100

    fix build

--- parte_3/script_3.py
+++ parte_3/script_3.py
@@ -6,10 +6,11 @@ from pathlib import Path
 subprocess.run("sudo apt install -y podman-docker", shell=True)
 subprocess.run("sudo systemctl enable --now podman.socket", shell=True)
 subprocess.run("sudo systemctl start podman.socket", shell=True)
+subprocess.run("echo 'unqualified-search-registries = [\"docker.io\"]' | sudo tee -a /etc/containers/registries.conf", shell=True)
 
 os.chdir("bookinfo/src/reviews")
 pwd = os.getcwd()
-subprocess.run(f"docker run --rm -u root -v {pwd}:/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build", shell=True)
+subprocess.run(f"sudo docker run --rm -u root -v {pwd}:/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build", shell=True)
 os.chdir("../../..")
 subprocess.run("sudo docker compose -f docker-compose.micro.yml build", shell=True)
 subprocess.run("sudo docker compose -f docker-compose.micro.yml up -d", shell=True)
