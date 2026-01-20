c3304a7de37ffc541cfa16cf718239079285f63f
marcosistoocommon <maperu2004@gmail.com>
2026-01-20 11:11:44 +0100

    fix3

--- parte_3/script_3.py
+++ parte_3/script_3.py
@@ -4,10 +4,12 @@ import sys
 from pathlib import Path
 
 subprocess.run("sudo apt install -y podman-docker", shell=True)
+subprocess.run("sudo systemctl enable --now podman.socket", shell=True)
+subprocess.run("sudo systemctl start podman.socket", shell=True)
 
 os.chdir("bookinfo/src/reviews")
 pwd = os.getcwd()
 subprocess.run(f"docker run --rm -u root -v {pwd}:/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build", shell=True)
 os.chdir("../../..")
-subprocess.run("docker compose -f docker-compose.micro.yml build", shell=True)
-subprocess.run("docker compose -f docker-compose.micro.yml up -d", shell=True)
+subprocess.run("sudo docker compose -f docker-compose.micro.yml build", shell=True)
+subprocess.run("sudo docker compose -f docker-compose.micro.yml up -d", shell=True)
