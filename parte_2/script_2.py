20951282447ef527715f1f6dcbaea03bf18d679c
marcosistoocommon <maperu2004@gmail.com>
2026-01-20 11:04:59 +0100

    sudo script 2

--- parte_2/script_2.py
+++ parte_2/script_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 import subprocess, os
 
-subprocess.run("docker build -t cdps-productpage:g17 .", shell=True)
-subprocess.run("docker run -p 9095:8080 -e TEAM_ID=17 -e APP_OWNER=Perez-et-al -d cdps-productpage:g17", shell=True)
+subprocess.run("sudo docker build -t cdps-productpage:g17 .", shell=True)
+subprocess.run("sudo docker run --name productpage_cdps_17 -p 9095:8080 -e TEAM_ID=17 -e APP_OWNER=Perez-et-al -d cdps-productpage:g17", shell=True)
