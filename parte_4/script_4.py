dda15cae03161fcd2aaebc2ca31d06a7f2a10fb2
marcosistoocommon <maperu2004@gmail.com>
2026-01-22 14:31:14 +0100

    sintaxis

--- parte_4/script_4.py
+++ parte_4/script_4.py
@@ -10,7 +10,7 @@ import sys
 import os
 
 TEAM_ID = "17"
-NAMESPACE = f"cdps-{TEAM_ID}"
+NAMESPACE = "cdps-{}".format(TEAM_ID)
 KUBE_DIR = "bookinfo/platform/kube"
 
 def run_command(command, description="", check=True, show_output=True):
